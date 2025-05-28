6866722bb0e15f644588f01b724e8e8ec5dbbcca
Joon Bak <jbdev98@gmail.com>
2025-05-28 15:35:58 +1000

    Add Functions

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, render_template
 
+import feedparser
+from transformers import pipeline
+
 app = Flask(__name__)
 
+### ROUTES ###
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -10,6 +14,21 @@ def index():
 
     return render_template('index.html')
 
+### FUNCTIONS ###
+def get_headlines(ticker):
+    url = f'https://finance.yahoo.com/rss/headline?s={ticker}'
+
+    feed = feedparser.parse(url)
+    news_headlines = []
+
+    if not feed.entries:
+        print(f'Error passing ticker: {ticker}')
+    else:
+        for entry in feed.entries:
+            news_headlines.append([entry.title, entry.link])
+
+def sentimental_analysis(headlines):
+    pipe = pipeline('text-classification', model='ProsusAI/finbert')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
